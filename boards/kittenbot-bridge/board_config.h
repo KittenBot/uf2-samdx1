7968b012ad86d5a1eb38c35ab9132bc8d2e85b3e
Riven <4771215@qq.com>
2019-07-03 21:23:53 +0800

    update pin define of kittenbridge

--- boards/kittenbot-bridge/board_config.h
+++ boards/kittenbot-bridge/board_config.h
@@ -1,6 +1,8 @@
 #ifndef BOARD_CONFIG_H
 #define BOARD_CONFIG_H
 
+#include "../../inc/configkeys.h"
+
 #define VENDOR_NAME "Kittenbot Teams"
 #define PRODUCT_NAME "Kittenbot Bridge"
 #define VOLUME_LABEL "Kitten-Bridge"
@@ -37,73 +39,70 @@
 
 // check configkeys.h for indexing
 
+// this temporary, update after arcade shield defined
 #ifdef DEFINE_CONFIG_DATA
 const uint32_t config_data[] = {
     /* CF2 START */
     513675505, 539130489, // magic
     62, 100,  // used entries, total entries
-    1, 0x2e, // PIN_ACCELEROMETER_INT = PB14
-    2, 0xd, // PIN_ACCELEROMETER_SCL = PIN_SCL
-    3, 0xc, // PIN_ACCELEROMETER_SDA = PIN_SDA
-    4, 0x3ee, // PIN_BTN_A = P_1006
-    5, 0x3ef, // PIN_BTN_B = P_1007
-    13, 0x17, // PIN_LED = PIN_D13
-    14, 0x24, // PIN_LIGHT = PB04
-    18, 0x36, // PIN_MISO = PB22
-    19, 0x37, // PIN_MOSI = PB23
-    20, 0xf, // PIN_NEOPIXEL = PA15
-    21, 0x11, // PIN_RX = PA17
-    23, 0x11, // PIN_SCK = PA17
-    24, 0xd, // PIN_SCL = PA13
-    25, 0xc, // PIN_SDA = PA12
-    26, 0x1b, // PIN_SPEAKER_AMP = PA27
-    28, 0x10, // PIN_TX = PA16
-    32, 0x2d, // PIN_DISPLAY_SCK = PB13
-    34, 0x2f, // PIN_DISPLAY_MOSI = PB15
-    35, 0x2c, // PIN_DISPLAY_CS = PB12
-    36, 0x25, // PIN_DISPLAY_DC = PB05
-    37, 0xa0, // DISPLAY_WIDTH = 160
-    38, 0x80, // DISPLAY_HEIGHT = 128
-    39, 0x80, // DISPLAY_CFG0 = 0x80
-    40, 0x12c2d, // DISPLAY_CFG1 = 0x12c2d
-    41, 0x18, // DISPLAY_CFG2 = 0x18
-    43, 0x0, // PIN_DISPLAY_RST = PA00
-    44, 0x1, // PIN_DISPLAY_BL = PA01
-    47, 0x4d7, // PIN_BTN_LEFT = P_1239
-    48, 0x473, // PIN_BTN_RIGHT = P_1139
-    49, 0x4d6, // PIN_BTN_UP = P_1238
-    50, 0x472, // PIN_BTN_DOWN = P_1138
-    51, 0x3ec, // PIN_BTN_MENU = P_1004
-    59, 0x40, // SPEAKER_VOLUME = 64
-    60, 0x17, // PIN_JACK_TX = PIN_D13
-    65, 0x2, // PIN_JACK_SND = PIN_A0
+    1, 0x21, // PIN_ACCELEROMETER_INT = PB01
+    2, 0x23, // PIN_ACCELEROMETER_SCL = PB03
+    3, 0x22, // PIN_ACCELEROMETER_SDA = PB02
+    13, 0x10, // PIN_LED = PA16
+    18, 0xe, // PIN_MISO = PA14
+    19, 0xc, // PIN_MOSI = PA12
+    20, 0xf, // PIN_NEOPIXEL = PB22
+    21, 0x17, // PIN_RX = PA23
+    23, 0xd, // PIN_SCK = PA13
+    24, 0x23, // PIN_SCL = PB03
+    25, 0x22, // PIN_SDA = PB02
+    28, 0x16, // PIN_TX = PA22
     69, 0x3ed, // PIN_BTN_SOFT_RESET = P_1005
-    70, 0x32, // ACCELEROMETER_TYPE = LIS3DH
-    71, 0x20, // PIN_BTNMX_LATCH = PB00
-    72, 0x3f, // PIN_BTNMX_CLOCK = PB31
-    73, 0x3e, // PIN_BTNMX_DATA = PB30
+    70, 0x68, // ACCELEROMETER_TYPE = MPU6050
     100, 0x2, // PIN_A0 = PA02
     101, 0x5, // PIN_A1 = PA05
-    102, 0x28, // PIN_A2 = PB08
-    103, 0x29, // PIN_A3 = PB09
-    104, 0x4, // PIN_A4 = PA04
-    105, 0x6, // PIN_A5 = PA06
-    152, 0x23, // PIN_D2 = PB03
-    153, 0x22, // PIN_D3 = PB02
-    154, 0xe, // PIN_D4 = PA14
-    155, 0x10, // PIN_D5 = PA16
-    156, 0x12, // PIN_D6 = PA18
-    159, 0x13, // PIN_D9 = PA19
-    160, 0x14, // PIN_D10 = PA20
-    161, 0x15, // PIN_D11 = PA21
-    162, 0x16, // PIN_D12 = PA22
-    163, 0x17, // PIN_D13 = PA23
-    200, 0x5, // NUM_NEOPIXELS = 5
+    102, 0x6, // PIN_A2 = PA06
+    103, 0x4, // PIN_A3 = PA04
+    104, 0x28, // PIN_A4 = PB08
+    105, 0x29, // PIN_A5 = PB09
+    150, 0x17, // PIN_D0 = PA23
+    151, 0x16, // PIN_D1 = PA22
+    152, 0x31, // PIN_D2 = PB17
+    153, 0x30, // PIN_D3 = PB16
+    154, 0x2D, // PIN_D4 = PB13
+    155, 0x2E, // PIN_D5 = PB14
+    156, 0x2F, // PIN_D6 = PB15
+    156, 0x2C, // PIN_D7 = PB12
+    156, 0x02, // PIN_D8 = PA02
+    159, 0x14, // PIN_D9 = PA20
+    160, 0x12, // PIN_D10 = PA18
+    161, 0x13, // PIN_D11 = PA19
+    162, 0x11, // PIN_D12 = PA17
+    163, 0x17, // PIN_D13 = PA16
+    200, 0x1, // NUM_NEOPIXELS = 1
     204, 0x80000, // FLASH_BYTES = 0x80000
     205, 0x30000, // RAM_BYTES = 0x30000
     208, 0x239a003d, // BOOTLOADER_BOARD_ID = 0x239a003d
     209, 0x55114460, // UF2_FAMILY = ATSAMD51
     210, 0x20, // PINS_PORT_SIZE = PA_32
+    // edge connector
+    CFG_PIN_P0, 0x2, // A0
+    CFG_PIN_P1, 0x5, // A1
+    CFG_PIN_P2, 0x16, // D1
+    CFG_PIN_P3, 0x4, // A3
+    CFG_PIN_P4, 0x28, // A4
+    CFG_PIN_P5, 0x29, // A5
+    CFG_PIN_P6, 0x2F, // D6
+    CFG_PIN_P7, 0x2C, // D7
+    CFG_PIN_P8, 0x02, // D8
+    CFG_PIN_P9, 0x14, // D9
+    CFG_PIN_P10, 0x17, // D0
+    CFG_PIN_P11, 0x13, // D11
+    CFG_PIN_P12, 0x11, // D12
+    CFG_PIN_P13, 0xd, // SCK
+    CFG_PIN_P14, 0xe, // MISO
+    CFG_PIN_P15, 0xc, // MOSI
+    CFG_PIN_P16, 0x12, // D10
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
